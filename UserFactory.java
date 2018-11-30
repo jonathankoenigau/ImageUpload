a42d42a99359dd287f8b7f2ba96c06d11039944f
Joseph Collins <josephcollins@mail.adelphi.edu>
2018-11-30 11:28:27 -0500

    removeFollower Method

--- UserFactory.java
+++ UserFactory.java
@@ -113,4 +113,35 @@ public class UserFactory{
             return false;
         }
     }
+
+public boolean removeFollower(String currentUser,String usertoFollow){
+   try{
+       File inputFile = new File("follow.txt");
+File tempFile = new File("myTempFile.txt");
+
+BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+String lineToRemove = usertoFollow;
+String currentLine;
+
+while((currentLine = reader.readLine()) != null) {
+    // trim newline when comparing with lineToRemove
+    String trimmedLine = currentLine.trim();
+    if(trimmedLine.equals(lineToRemove)) continue;
+    writer.write(currentLine + System.getProperty("line.separator"));
+}
+writer.close(); 
+reader.close(); 
+boolean successful = tempFile.renameTo(inputFile);
+    
+    
+    return true;
+}catch(IOException ioe){
+                    System.out.println("Exception occurred:");
+                    ioe.printStackTrace();
+                    return false;
+
+}
+}
 }
